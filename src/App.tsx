89b57132641ddcd483911a24a7f48f0342f68104
Kern Rocke <44078438+kernrocke@users.noreply.github.com>
2026-03-10 16:19:24 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ShinyApps from "./pages/ShinyApps";
 import NotFound from "./pages/NotFound";
@@ -14,14 +14,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/kernrocke-portfolio/">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/shiny-apps" element={<ShinyApps />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
