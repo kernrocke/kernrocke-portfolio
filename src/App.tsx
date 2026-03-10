ed55c51a26d523565988014e58a6d1eeab4aa142
Kern Rocke <44078438+kernrocke@users.noreply.github.com>
2026-03-10 16:17:05 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/kernrocke-portfolio/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/shiny-apps" element={<ShinyApps />} />
